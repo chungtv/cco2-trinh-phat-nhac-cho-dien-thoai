7c12fafa6e7e6d0484ff27786e9f7431ed89829a
hodung12 <hodung12@72aa8246-f705-ff52-a6f9-47178846cec3>
2010-11-16 03:35:52 +0000

    ok

--- trunk/R.java
+++ trunk/R.java
@@ -48,5 +48,15 @@ public final class R {
         public static final int media_palyer=0x7f030001;
         public static final int progressbar_1=0x7f030002;
     }
-    
+    public static final class string {
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
+        public static final int local_service_disconnected=0x7f040008;
+        public static final int local_service_label=0x7f040007;
+        public static final int media_service_started=0x7f040006;
+        public static final int progressbar_1_default_progress=0x7f040002;
+        public static final int progressbar_1_minus=0x7f040003;
+        public static final int progressbar_1_plus=0x7f040004;
+        public static final int progressbar_1_secondary_progress=0x7f040005;
+    }
 }
